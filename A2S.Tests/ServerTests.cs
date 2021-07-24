cf67fd2cbbc7fa645c8d0e791f26421259680323
Jason Thompson <crytilis@gmail.com>
2021-07-23 22:37:47 -0500

    Initial commit

---
+++ A2S.Tests/ServerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace A2S.Tests
+{
+    [TestClass()]
+    public class ServerTests
+    {
+        [TestMethod()]
+        public void QueryTest()
+        {
+            var info = Server.Query("5.101.166.197", 27021, 15);
+            Console.WriteLine(info.Name);
+        }
+
+        [TestMethod()]
+        public void QueryTestTimeout()
+        {
+            var info = Server.Query("5.101.166.199", 27021, 15);
+            Console.WriteLine(info);
+        }
+    }
+}
